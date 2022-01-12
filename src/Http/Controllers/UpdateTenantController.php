a55cdd5f6e4d2d2368a75d9db8b260428ad82b5a
Robert Rimoczi <robert@dcodegroup.com>
2022-01-12 13:35:37 +0100

    add UpdateTenantController

---
+++ src/Http/Controllers/UpdateTenantController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dcodegroup\LaravelMyobOauth\Http\Controllers;
+
+use Dcodegroup\LaravelMyobOauth\Models\MyobToken;
+use Dcodegroup\LaravelMyobOauth\MyobService;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Arr;
+
+class UpdateTenantController extends Controller
+{
+    public function __invoke(MyobService $myobService, $id)
+    {
+        $details = $myobService->getCompanyDetails($id);
+
+        MyobToken::latestToken()->update([
+            'current_tenant_id' => Arr::get($details, 'CompanyFile.Uri')
+        ]);
+
+        return redirect()->back();
+    }
+}
