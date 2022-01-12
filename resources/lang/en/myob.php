3ee338b4793542f2f18dbb777f7d2ba55f71f1ff
Robert Rimoczi <robert@dcodegroup.com>
2022-01-12 13:34:11 +0100

    extend translations

--- resources/lang/en/myob.php
+++ resources/lang/en/myob.php
@@ -5,6 +5,7 @@ return [
         'header'   => 'MYOB Status',
         'tenants'  => 'Tenants',
         'accounts' => 'Accounts',
+        'current_tenant' => 'Current Tenant'
     ],
     'status' => [
         'unauthorized' => 'Token has been expired or MYOB is not authorized',
@@ -14,4 +15,7 @@ return [
         'authorize' => 'Authorize',
         'select' => 'Select',
     ],
+    'message' => [
+        'no_tenant_selected' => 'To use MYOB, please select a company from below.'
+    ]
 ];
